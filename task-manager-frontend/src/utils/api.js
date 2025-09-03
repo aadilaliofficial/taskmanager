4f2f79fd3ce93b9a005499a4bc559ad1832adde4
Aadil Ali <61555486+aadilaliofficial@users.noreply.github.com>
2025-09-03 15:38:49 +0530

    Update api.js

--- task-manager-frontend/src/utils/api.js
+++ task-manager-frontend/src/utils/api.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://taskmanager-6zba.onrender.com", 
+  baseURL: "https://taskmanager-6zba.onrender.com/api", 
 });
 
 // attach token automatically
@@ -15,3 +15,4 @@ API.interceptors.request.use((req) => {
 
 
 export default API;
+

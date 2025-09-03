e5e3793ffa04c1269094b32c5e3f73c483a9108c
Aadil Ali <61555486+aadilaliofficial@users.noreply.github.com>
2025-09-03 15:54:51 +0530

    Update Login.jsx

--- task-manager-frontend/src/pages/Login.jsx
+++ task-manager-frontend/src/pages/Login.jsx
@@ -12,7 +12,7 @@ export default function Login() {
 
   const handleLogin = async (e) => {
     e.preventDefault();
-    const res = await API.post("/auth/login", { email, password });
+    const res = await API.post("api/auth/login", { email, password });
     localStorage.setItem("token", res.data.token);
     navigate("/tasks");
   };
@@ -29,4 +29,5 @@ export default function Login() {
       </form>
     </div>
   );
+
 }

efae6357bfbcfa2d85a3664542b969480a0e7353
Aadil Ali <61555486+aadilaliofficial@users.noreply.github.com>
2025-09-03 16:18:05 +0530

    Update Login.jsx

--- task-manager-frontend/src/pages/Login.jsx
+++ task-manager-frontend/src/pages/Login.jsx
@@ -26,7 +26,7 @@ export default function Login() {
     <div className="flex h-screen items-center justify-center relative">
       {/* Top-right Signup Button */}
       <button
-        onClick={() => navigate("/signup")}
+        onClick={() => navigate("/")}
         className="absolute top-4 right-4 bg-blue-500 text-white px-4 py-2 rounded"
       >
         Sign Up
@@ -58,3 +58,4 @@ export default function Login() {
     </div>
   );
 }
+

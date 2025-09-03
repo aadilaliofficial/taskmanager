3458f2aa49123bfe6b972d5e0d5f32dd431ef8ec
Aadil Ali <61555486+aadilaliofficial@users.noreply.github.com>
2025-09-03 16:10:11 +0530

    Update auth.js

--- task-manager-backend/routes/auth.js
+++ task-manager-backend/routes/auth.js
@@ -1,3 +1,12 @@
+//routes auth.js
+
+import express from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+const router = express.Router();
+
 // Signup
 router.post("/signup", async (req, res) => {
   try {
@@ -21,3 +30,22 @@ router.post("/signup", async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 });
+
+// Login
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "User not found" });
+
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) return res.status(400).json({ message: "Invalid password" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+    res.json({ token });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+export default router;
